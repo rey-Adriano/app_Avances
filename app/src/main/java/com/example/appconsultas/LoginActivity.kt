c5ffcb1233fae0a34b659aec4fa14131160a92dc
nombre <correo123@udh.edu.pe>
2023-07-01 17:05:58 -0500

    hito 1

---
+++ app/src/main/java/com/example/appconsultas/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.example.appconsultas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login2)
+    }
+
+    fun iniciar(view: View) {}
+}
---
+++ app/src/main/java/com/example/appconsultas/RegistrarseActivity.java
@@ -0,0 +1,6 @@
+package com.example.appconsultas;
+
+import android.app.Activity;
+
+public class RegistrarseActivity extends Activity {
+}
